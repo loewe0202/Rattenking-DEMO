3a8d8022023612e0599cfbb944088035c806cdc5
Rattenking <1148063373@qq.com>
2017-10-16 16:40:17 +0800

    vue入门demo

---
+++ vue/assets/path.js
@@ -0,0 +1,35 @@
+var URL = './example/'
+var URLJSON = [
+    {
+        name : 'Hello World 实例',
+        url : URL + 'helloworld.html'
+    },
+    {
+        name : 'v-if & v-else & v-show 实例',
+        url : URL + 'v-if.html'
+    },
+    {
+        name : 'v-for 实例',
+        url : URL + 'v-for.html'
+    },
+    {
+        name : 'v-text & v-html 实例',
+        url : URL + 'v-text.html'
+    },
+    {
+        name : 'v-on 实例',
+        url : URL + 'v-on.html'
+    },
+    {
+        name : 'v-model 实例',
+        url : URL + 'v-model.html'
+    },
+    {
+        name : 'v-bind 实例',
+        url : URL + 'v-bind.html'
+    },
+    {
+        name : 'v-pre & v-cloak & v-once 实例',
+        url : URL + 'v-pre.html'
+    }
+]
